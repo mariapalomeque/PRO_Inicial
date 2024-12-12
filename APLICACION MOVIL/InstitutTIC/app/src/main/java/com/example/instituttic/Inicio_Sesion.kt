9c3d1461ab37996d44d53f81fbc78a2ce69a9ca0
PauChacon <2023_pau.chacon@iticbcn.cat>
2024-12-12 14:47:33 +0100

    mas cosas

--- APLICACION MOVIL/InstitutTIC/app/src/main/java/com/example/instituttic/Inicio_Sesion.kt
+++ APLICACION MOVIL/InstitutTIC/app/src/main/java/com/example/instituttic/Inicio_Sesion.kt
@@ -1,31 +1,20 @@
 package com.example.instituttic
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class Inicio_Sesion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        // Configurar WindowInsets para la vista principal
+        setContentView(R.layout.inicio_sesion)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Configurar el Intent para navegar a Inicio_Sesion
-        val btnLogin = findViewById<Button>(R.id.but_Alum)
-        btnLogin.setOnClickListener {
-            val intent = Intent(this, Inicio_Sesion::class.java)
-            startActivity(intent)
         }
     }
 }
